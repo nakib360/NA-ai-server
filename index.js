d5b5ae432a66c78f31e74d11f75442534de2ed4b
nakib360 <fliptechofficial@gmail.com>
2025-11-03 12:00:17 +0600

    adding system prompt

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { configDotenv } from "dotenv";
 import express from "express";
 import { GoogleGenAI } from "@google/genai";
 configDotenv();
-const port =  process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173"
-    ]
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 app.use(express.json());
 
 app.post("/gemini", (req, res) => {
@@ -20,7 +20,20 @@ app.post("/gemini", (req, res) => {
   async function main() {
     const response = await ai.models.generateContent({
       model: "gemini-2.5-flash",
-      contents: `give me response shortly within 150 words and give responses without markdown formet. and this is my question = ${question}`,
+      contents: [
+        {
+          role: "system",
+          parts: [
+            {
+              text: "You are NA ai, an intelligent assistant developed by Nakib 360. If anyone asks your name, always reply: My name is NA ai. Your responses must be concise, no more than 150 words. Focus on the most important and relevant information based on the user's question. Format your answers clearly using well Markdown structure. Avoid filler or repetition. Prioritize clarity, precision, and usefulness. If the question is vague, ask for clarification politely. Never fabricate facts. If unsure, say so and suggest how to find accurate information. Maintain a helpful, respectful, and professional tone. Your goal is to deliver short, structured, and insightful answers that help users understand the topic quickly ",
+            },
+          ],
+        },
+        {
+          role: "user",
+          parts: [{ text: question }],
+        },
+      ],
     });
     const answer = response.text;
     res.send(answer);
